5bf5dd272115bb40529e6ecf8ffaa2f05adfe7d2
tejseth <55564043+tejseth@users.noreply.github.com>
2021-06-23 16:58:14 -0400

    Add files via upload

--- ryoe-2.R
+++ ryoe-2.R
@@ -28,9 +28,9 @@ library(gganimate)
 library(gifski)
 library(ggcorrplot)
 library(zoo)
+library(ggbeeswarm)
 options(scipen = 9999)
 rushing_data <- read.csv("~/Downloads/rushing_data.csv")
-
 
 colSums(is.na(rushing_data))
 
@@ -76,47 +76,68 @@ rushing_data <- rushing_data %>%
     TRUE ~ defense
   )) %>%
   ungroup()
-  
+
+rushing_data <- rushing_data %>%
+  filter(quarter < 5)
+
+rushing_data <- rushing_data %>%
+  filter(run_position != "QBK")
+
+rushing_data <- rushing_data %>%
+  mutate(seconds_left_in_half = case_when(
+    quarter == 1 ~ as.integer(seconds_left_in_quarter + 900),
+    quarter == 2 ~ as.integer(seconds_left_in_quarter),
+    quarter == 3 ~ as.integer(seconds_left_in_quarter + 900),
+    quarter == 4 ~ as.integer(seconds_left_in_quarter),
+  ))
+
+rushing_data <- rushing_data %>%
+  mutate(half = case_when(
+    quarter <= 2 ~ "First Half",
+    quarter > 2 ~ "Second Half"
+  ))
 
 rushing_data %>% 
-  filter(season > 2015) %>%
+  filter(season > 2010) %>%
   filter(!is.na(box_players)) %>%
   filter(box_players < 9) %>%
   filter(box_players > 4) %>%
-  ggplot(aes(x = yards_to_go, y = yards, group = as.factor(box_players), color = as.factor(box_players))) +
+  filter(quarter < 5) %>%
+  ggplot(aes(x = seconds_left_in_half, y = yards, group = as.factor(box_players), color = as.factor(box_players))) +
   geom_smooth(se = FALSE, size = 2) +
   theme_reach() +
   scale_color_brewer(palette = "Paired") +
-  labs(x = "Yards From Endzone",
+  labs(x = "Seconds Left in Half",
        y = "Yards Gained",
-       title = "How Rushing Yards Changes by Yardline and Defenders in the Box",
-       subtitle = "Using PFF data from 2015-2020",
+       title = "How Rushing Yards Changes by Time and Defenders in the Box",
+       subtitle = "Every designed rush from 2010 to 2020",
        color = "Box Players") +
-  scale_x_continuous(breaks = pretty_breaks(n = 10)) +
+  scale_x_reverse(breaks = pretty_breaks(n = 10)) +
   scale_y_continuous(breaks = pretty_breaks(n = 10)) +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom") +
+  facet_wrap(~half)
 
 colSums(is.na(rushing_data))
 
 def_ypc <- rushing_data %>%
-  group_by(defense, season) %>%
-  summarize(def_ypc = mean(yards))
+  dplyr::group_by(defense, season) %>%
+  dplyr::summarize(def_ypc = mean(yards))
 
 rushing_data_join <- rushing_data %>%
   left_join(def_ypc, by = c("defense", "season"))
 
 rushing_data_join <- rushing_data_join %>%
-  filter(!is.na(box_players)) %>%
-  select(offense, defense, player, player_id, season, game_id, distance, down, 
-         quarter, seconds_left_in_quarter, yards_to_go, yards, box_players, def_ypc, score_diff)
+  dplyr::filter(!is.na(box_players)) %>%
+  dplyr::select(offense, defense, player, player_id, season, game_id, concept_1, distance, down, 
+         quarter, seconds_left_in_half, yards_to_go, yards, box_players, def_ypc, score_diff)
 
 colSums(is.na(rushing_data_join))
 
 rushing_model_data <- rushing_data_join %>%
-  select(distance, down, seconds_left_in_quarter, yards_to_go, 
+  select(distance, down, seconds_left_in_half, yards_to_go, 
          yards, box_players, def_ypc, score_diff) %>%
-  rename(label = yards) %>%
-  select(label, everything())
+  dplyr::rename(label = yards) %>%
+  dplyr::select(label, everything())
 
 smp_size <- floor(0.70 * nrow(rushing_model_data))
 set.seed(2011) #go lions
@@ -125,6 +146,7 @@ train <- as.matrix(rushing_model_data[ind, ])
 test <- as.matrix(rushing_model_data[-ind, ])
 
 dim(train)
+colnames(train)
 
 ryoe_model <-
   xgboost(
@@ -148,8 +170,8 @@ yhat <- pred_xgb
 y <- test[, 1]
 postResample(yhat, y)
 
-hyper_grid <- expand.grid(max_depth = seq(3, 6, 1),
-                          eta = seq(.2, .35, .01))
+hyper_grid <- expand.grid(max_depth = seq(3, 5, 1),
+                          eta = seq(.2, .28, .01))
 xgb_train_rmse <- NULL
 xgb_test_rmse <- NULL
 
@@ -182,8 +204,8 @@ rushing_model <-
     nrounds = 1000,
     objective = "reg:squarederror",
     early_stopping_rounds = 3,
-    max_depth = 4, #ideal max depth
-    eta = 0.2 #ideal eta
+    max_depth = 3, #ideal max depth
+    eta = 0.25 #ideal eta
   )   
 
 imp <- xgb.importance(colnames(train), model = rushing_model)
@@ -213,13 +235,15 @@ ryoe_projs <- ryoe_projs %>%
 write.csv(ryoe_projs, 'ryoe_projs.csv')
 
 rusher_seasons <- ryoe_projs %>%
-  group_by(player, player_id, season, offense) %>%
-  summarize(rushes = n(),
-            avg_ryoe = mean(ryoe)) %>%
-  filter(rushes >= 80) %>%
-  arrange(season) %>%
-  group_by(player) %>%
-  mutate(next_rushes = lead(rushes),
+  dplyr::group_by(player, player_id, season, offense) %>%
+  dplyr::summarize(rushes = n(),
+            avg_ryoe = mean(ryoe),
+            los_rate = sum(ryoe > 0) / rushes,
+            explosive_rate = sum(ryoe > 10) / rushes) %>%
+  dplyr::filter(rushes >= 100) %>%
+  dplyr::arrange(season) %>%
+  dplyr::group_by(player) %>%
+  dplyr::mutate(next_rushes = lead(rushes),
          next_avg_ryoe = lead(avg_ryoe)) %>%
   left_join(teams_colors_logos, by = c("offense" = "team_abbr"))
 
@@ -232,11 +256,13 @@ rushers_19 <- rusher_seasons %>%
   filter(season == 2019) 
 
 rushers_all_time <- ryoe_projs %>%
-  group_by(player, player_id) %>%
-  summarize(rushes = n(),
-            avg_ryoe = mean(ryoe)) %>%
-  filter(rushes >= 250) %>%
-  arrange(-avg_ryoe)
+  dplyr::group_by(player, player_id) %>%
+  dplyr::summarize(rushes = n(),
+            nfl_ryoe = mean(ryoe),
+            nfl_los_rate = sum(ryoe > 0) / rushes,
+            nfl_explosive_rate = sum(ryoe > 10) / rushes) %>%
+  dplyr::filter(rushes >= 400) %>%
+  dplyr::arrange(-nfl_ryoe)
 
 next_gen_ryoe <- read.csv("~/Downloads/next_gen_ryoe.csv")
 
@@ -285,49 +311,13 @@ rushers_19 %>%
         axis.title.y = element_markdown(size=14))
 ggsave('rusher-19-20.png', width = 14, height = 10, dpi = "retina")
 
-rushing_data %>% 
-  filter(season > 2018) %>% 
-  filter(offense == "DAL") %>% 
-  group_by(player) %>% 
-  summarize(count = n(),
-            mean_yoc = sum(fumbles, na.rm = T) / count) %>% 
-  filter(count > 80)
-
-rushing_data <- rushing_data %>%
-  mutate(yards_before_contact = yards - yards_after_contact)
-
-stats_20 <- rushing_data %>%
-  filter(season == 2020) %>%
-  group_by(offense) %>%
-  summarize(ybc = mean(yards_before_contact, na.rm = T),
-            yac = mean(yards_after_contact, na.rm = T)) %>%
-  left_join(teams_colors_logos, by = c("offense" = "team_abbr"))
-
-stats_20 %>% 
-  ggplot(aes(x = ybc, y = yac)) +
-  geom_image(aes(image = team_logo_espn), size = 0.05, asp = 16/9) +
-  geom_hline(yintercept = mean(stats_20$yac), linetype = "dashed", alpha = 0.7) +
-  geom_vline(xintercept = mean(stats_20$ybc), linetype = "dashed", alpha = 0.7) +
-  theme_reach() +
-  labs(x = "Average Yards <span style= 'color:purple'>Before</span> Contact",
-       y = "Average Yards <span style= 'color:blue'>After</span> Contact",
-       title = "Each Team's Yards <span style= 'color:purple'>Before</span> Contact and Yards <span style= 'color:blue'>After</span> Contact When Rushing in 2020",
-       caption = "By Tej Seth | @mfbanalytics | @PFF") +
-  theme(plot.title = element_markdown(size = 20, hjust = 0.5, face = "bold"),
-        plot.subtitle = element_markdown(size = 12, hjust = 0.5),
-        axis.title.x = element_markdown(size=16),
-        axis.title.y = element_markdown(size=16)) +
-  scale_x_continuous(breaks = pretty_breaks(n = 10)) +
-  scale_y_continuous(breaks = pretty_breaks(n = 10)) 
-ggsave('ybc.png', width = 14, height = 10, dpi = "retina")
-
 rates <- ryoe_projs %>%
-  filter(season == 2020) %>%
-  group_by(player, offense) %>%
-  summarize(los_rate = sum(ryoe < 0) / n(),
+  dplyr::filter(season == 2020) %>%
+  dplyr::group_by(player, offense) %>%
+  dplyr::summarize(los_rate = sum(ryoe < 0) / n(),
             explosive_rate = sum(ryoe > 10) / n(),
             rushes = n()) %>% 
-  filter(rushes >= 100) %>%
+  dplyr::filter(rushes >= 100) %>%
   left_join(teams_colors_logos, by = c("offense" = "team_abbr"))
 
 rates %>%
@@ -344,10 +334,103 @@ rates %>%
   labs(x = "Bad Run Rate (RYOE < 0)",
        y = "Explosive Run Rate (RYOE > 10)",
        title = "Rushers That are Explosive and Best at Avoiding Bad Runs, 2020",
-       subtitle = "RYOE = Rushing Yards Over Expected, minimum of 100 rushes")
+       subtitle = "RYOE = Rushing Yards Over Expected, minimum of 100 rushes",
+       caption = "By Tej Seth | @mfbanalytics | @PFF")
+ggsave('kamara.png', width = 14, height = 10, dpi = "retina")
+
+ryoe_projs <- ryoe_projs %>%
+  dplyr::mutate(half = case_when(
+    quarter <= 2 ~ "First Half",
+    quarter > 2 ~ "Second Half"
+  ))
   
+ryoe_projs %>% 
+  filter(quarter < 5) %>%
+  filter(!is.na(down)) %>%
+  filter(down > 0) %>%
+  ggplot(aes(x = seconds_left_in_half, y = exp_yards, group = as.factor(down), color = as.factor(down))) +
+  geom_smooth(se = FALSE, size = 2) +
+  theme_reach() +
+  scale_color_brewer(palette = "Paired") +
+  labs(x = "Seconds Left in Half",
+       y = "Expected Rushing yards",
+       title = "How NFL Expected Rushing Yards Changes by Time and Down",
+       subtitle = "Using PFF data from 2006-2020",
+       color = "Down") +
+  scale_x_reverse(breaks = pretty_breaks(n = 10)) +
+  scale_y_continuous(breaks = pretty_breaks(n = 10)) +
+  theme(legend.position = "bottom") +
+  facet_wrap(~half)
 
+run_concepts <- c("Inside Zone", "Outside Zone", "Power", "Man", "Pull Lead", "Draw", "Counter", "Trap")
 
+nfl_concept_stats <- ryoe_projs %>%
+  filter(ryoe < 30) %>%
+  filter(concept_1 %in% run_concepts) %>%
+  filter(season > 2011) %>%
+  group_by(concept_1) %>%
+  summarize(nfl_count = n(),
+            mean_ryoe = mean(ryoe),
+            median_ryoe = median(ryoe)) %>%
+  mutate(nfl_logo = "https://brandslogos.com/wp-content/uploads/images/large/nfl-logo.png")
+
+ryoe_projs %>% 
+  filter(season > 2011) %>%
+  filter(ryoe < 15) %>%
+  filter(ryoe > -5) %>%
+  filter(concept_1 %in% run_concepts) %>%
+  ggplot(aes(x = concept_1, y = ryoe)) +
+  geom_jitter(alpha = 0.02, width = 0.2) +
+  geom_boxplot(aes(fill = concept_1)) +
+  scale_fill_brewer(palette = "Paired") +
+  scale_y_continuous(breaks = pretty_breaks(n = 10)) +
+  theme_reach() +
+  labs(x = "Run Type",
+       y = "RYOE",
+       title = "How Run Type Affects Rushing Yards Over Expected",
+       subtitle = "2012-2020, eight most common run types used")
+
+ncaa_concept_stats <- ncaa_ryoe_projs %>%
+  filter(ryoe < 30) %>%
+  filter(concept_1 %in% run_concepts) %>%
+  filter(season > 2011) %>%
+  group_by(concept_1) %>%
+  summarize(ncaa_count = n(),
+            ncaa_mean_ryoe = mean(ryoe) + 0.31,
+            ncaa_median_ryoe = median(ryoe)) %>%
+  mutate(ncaa_logo = "https://cdn.freebiesupply.com/logos/large/2x/ncaa-4-logo-png-transparent.png")
+
+concept_stats <- nfl_concept_stats %>%
+  left_join(ncaa_concept_stats, by = c("concept_1")) %>%
+  arrange(desc(nfl_count)) %>%
+  mutate(rank = row_number())
+
+concept_stats %>%
+  mutate(concept_1 = fct_reorder(concept_1, nfl_count)) %>%
+  ggplot() +
+  geom_link(
+    mapping = aes(x = mean_ryoe, y = concept_1, xend = ncaa_mean_ryoe, yend = concept_1, size = 2, color = "lightblue")
+  ) +
+  theme_reach() +
+  scale_colour_identity() +
+  geom_image(aes(x = mean_ryoe, y = concept_1, image = "https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/National_Football_League_logo.svg/1200px-National_Football_League_logo.svg.png"), size = 0.05, asp = 16/9) +
+  geom_image(aes(x = ncaa_mean_ryoe, y = concept_1, image = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/NCAA_logo.svg/1200px-NCAA_logo.svg.png"), size = 0.05, asp = 16/9) +
+  labs(
+    x = "Average RYOE",
+    y = "Run Concept",
+    title = "The Average Rushing Yards Over Expected of Each Run Concept, NCAA vs. NFL",
+    subtitle = "NFL logo for RYOE/attempt in NFL and NCAA logo for RYOE/attempt in college, ordered in terms of usage"
+  ) +
+  theme(
+    axis.ticks.y=element_blank(),
+    axis.text.y = element_text(size = 13),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.background = element_blank(),
+    panel.border= element_blank()
+  ) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10))
+ggsave('rushing-2.png', width = 14, height = 10, dpi = "retina")
 
 
 

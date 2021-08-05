82d8679058cf873863eda07cb83c8f46d38f1404
tejseth <55564043+tejseth@users.noreply.github.com>
2021-08-05 17:38:51 -0400

    Add files via upload

---
+++ rb-aging.R
@@ -0,0 +1,112 @@
+nfl_rbs <- unique(ryoe_projs$player_id)
+
+ncaa_carries <- ncaa_ryoe_projs %>%
+  filter(player_id %in% nfl_rbs) %>%
+  dplyr::select(player, player_id, season, ryoe, league)
+
+nfl_carries <- ryoe_projs %>%
+  filter(season >= 2014) %>%
+  dplyr::select(player, player_id, season, ryoe, league)
+
+nfl_full_carries <- ryoe_projs %>%
+  dplyr::select(player, player_id, season, ryoe, league) %>%
+  janitor::clean_names()
+
+carries <- rbind(ncaa_carries, nfl_carries)
+
+carries$rush_num <- ave(carries$ryoe, carries$player_id, FUN = seq_along)
+
+carries$csum <- ave(carries$ryoe, carries$player_id, FUN=cumsum)
+
+group <- carries %>%
+  group_by(rush_num) %>%
+  summarize(count = n(),
+            avg_ryoe = mean(ryoe),
+            avg_csum = mean(csum)) %>%
+  filter(count >= 10)
+
+group %>%
+  ggplot(aes(x = rush_num, y = avg_ryoe)) +
+  geom_point(aes(size = count, fill = -rush_num), color = "black", shape = 21, alpha = 0.2) +
+  scale_fill_viridis_c() +
+  geom_smooth(size = 3, color = "black") +
+  theme_reach() +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  labs(x = "Career Rush Number",
+       y = "Average Rushing Yards Over Expected",
+       title = "Running Backs Don't Have a Severe Drop-off Over 1,000 Carries",
+       subtitle = "2014-2020, college carries included")
+
+draft <- espnscrapeR::get_sharpe_data(dataset = "draft_picks")
+
+draft_select <- draft %>%
+  filter(position == "RB") %>%
+  dplyr::select(pfr_name, season, round) %>%
+  rename(draft_season = season)
+
+nfl_full_carries <- nfl_full_carries %>%
+  left_join(draft_select, by = c("player" = "pfr_name"))
+
+nfl_full_carries %<>%
+  mutate(years_in_nfl = season - draft_season + 1)
+
+nfl_full_carries %<>%
+  filter(years_in_nfl > 0 & years_in_nfl < 15)
+
+years <- nfl_full_carries %>%
+  filter(!is.na(years_in_nfl)) %>%
+  group_by(player, years_in_nfl) %>%
+  summarize(rushes = n(),
+            avg_ryoe = mean(ryoe)) %>%
+  filter(rushes >= 100)
+
+years <- years %>%
+  mutate(Label = case_when(
+    avg_ryoe < -0.75 ~ "Bad Season",
+    avg_ryoe >= -0.75 & avg_ryoe < 0 ~ "Below-Average Season",
+    avg_ryoe >= 0 & avg_ryoe < 0.75 ~ "Above-Average Season",
+    avg_ryoe >= 0.75 ~ "Elite Season"
+  ))
+
+years %>%
+  ggplot(aes(x = as.factor(years_in_nfl), y = avg_ryoe, fill = Label)) + 
+  geom_quasirandom(pch = 21, size = 4.5) + 
+  scale_fill_viridis_d() +
+  theme_reach() +
+  geom_hline(yintercept = 0, color = "black", alpha=1.0) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+  ggrepel::geom_text_repel(aes(label = player), max.overlaps = 3, size = 4, box.padding = 0.3) +
+  labs(
+    x = "Years in NFL",
+    y = "Average RYOE",
+    title = "It Takes Until Year 7 in the NFL for Elite Running Backs to Become Rare",
+    subtitle = "RYOE = Rushing Yards Over Expected, minimum of 100 rushes to qualify") +
+  theme(panel.grid.major.x = element_line(size = 0.2))
+
+years_no_players <- nfl_full_carries %>%
+  filter(!is.na(years_in_nfl)) %>%
+  group_by(years_in_nfl) %>%
+  summarize(rushes = n(),
+            avg_ryoe = mean(ryoe)) %>%
+  mutate(nfl_logo = "https://www.freepnglogos.com/uploads/nfl-logo-png/logo-nfl-6.png",
+         adj_count = prettyNum(rushes,big.mark=",",scientific=FALSE)) %>%
+  filter(rushes >= 1000)
+
+years_no_players %>%
+  ggplot(aes(x = as.factor(years_in_nfl), y = avg_ryoe)) +
+  geom_bar(aes(fill = avg_ryoe), stat = "identity") +
+  geom_text(aes(y = ifelse(avg_ryoe > 0, avg_ryoe + 0.01, avg_ryoe - 0.01), label = adj_count), size = 4) +
+  theme_reach() +
+  scale_fill_viridis_c() +
+  theme(panel.grid.major.x = element_line(size = 0.2)) +
+  labs(x = "Years in NFL",
+       y = "Average RYOE",
+       title = "Running Backs Tend to Have Negative RYOE After 7 Years in the NFL",
+       subtitle = "RYOE = Rushing Yards Over Expected, text is amount of rushes")
+
+
+
+
+
+
+
